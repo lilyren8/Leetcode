92a765b0527423fabea14263f5e2e96c5a8611cf
lilyren8 <lilyren8@gmail.com>
2021-05-07 20:22:51 -0400

    update

---
+++ 1571. Warehouse Manager.sql
@@ -0,0 +1,5 @@
+SELECT w.name AS warehouse_name, SUM(p.Width * p.Length * p.Height * w.units) AS volume
+FROM Warehouse AS w
+JOIN Products AS p
+ON w.product_id = p.product_id
+GROUP BY w.name;
---
+++ 1623. All Valid Triplets That Can Represent a Country.sql
@@ -0,0 +1,11 @@
+SELECT a.student_name AS member_A, b.student_name AS member_B, c.student_name AS member_C
+FROM SchoolA AS a
+CROSS JOIN SchoolB AS b
+CROSS JOIN SchoolC AS c
+WHERE a.student_name <> b.student_name AND
+a.student_name <> c.student_name AND
+b.student_name <> c.student_name AND
+a.student_id <> c.student_id AND
+a.student_id <> b.student_id AND
+b.student_id <> c.student_id;
+-- cross join produces all possible combinations
